d3a5f3f6ee75684b1c1653ba6d9c8648e8c73032
Dominik Völkel <voelkel.dominik@gmail.com>
2024-03-01 16:10:35 +0100

    ci: fully implementing semantic release

---
+++ release.config.cjs
@@ -0,0 +1,102 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ['main', 'next'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          {
+            breaking: true,
+            release: 'major',
+          },
+          {
+            type: 'feat',
+            release: 'minor',
+          },
+          {
+            type: 'fix',
+            release: 'patch',
+          },
+          {
+            type: 'docs',
+            scope: 'README',
+            release: 'patch',
+          },
+          {
+            type: 'chore',
+            release: 'patch',
+          },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            {
+              type: 'feat',
+              section: 'Features',
+              hidden: false,
+            },
+            {
+              type: 'fix',
+              section: 'Bug Fixes',
+              hidden: false,
+            },
+            {
+              type: 'docs',
+              section: 'Miscellaneous Chores',
+              hidden: false,
+            },
+            {
+              type: 'chore',
+              section: 'Miscellaneous Chores',
+              hidden: false,
+            },
+            {
+              type: 'ci',
+              section: 'Continuous Integration',
+              hidden: false,
+            },
+          ],
+        },
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/gitlab',
+      {
+        gitlabUrl: 'https://gitlab.zweieuro.at',
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+      },
+    ],
+  ],
+};
