a6a3ad4a169f757a2284f0eff33c07ae39e7fa85
Dominik Völkel <voelkel.dominik@gmail.com>
2024-03-02 16:12:44 +0100

    fix(ci): try adding registry into package json

--- release.config.cjs
+++ release.config.cjs
@@ -95,7 +95,9 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'CHANGELOG.md'],
+        assets: ['package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
     '@semantic-release/npm',
